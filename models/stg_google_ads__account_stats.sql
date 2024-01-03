87466ce124616e1b2ad8383efa44d155478dc0ff
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-01-03 12:18:32 +0530

    Update stg_google_ads__account_stats.sql

--- models/stg_google_ads__account_stats.sql
+++ models/stg_google_ads__account_stats.sql
@@ -36,7 +36,7 @@ final as (
     select
         source_relation, 
         customer_id as account_id, 
-        date as date_day,
+        DATE(TIMESTAMP(date, "America/New_York")) AS date_day,
         ad_network_type,
         device,
         clicks, 
